2014f29fa5feaf16da702f3d43c91b5a4259d68d
MdeKool <matthijsdekool@gmail.com>
2022-11-11 12:36:19 +0100

    Added constructor, addTruck() and removeTruck() for Highway, fields for City

--- src/Restocking/City.java
+++ src/Restocking/City.java
@@ -1,4 +1,9 @@
 package Restocking;
 
+import java.util.List;
+
 public class City {
+    private List<Highway> incoming;
+    private List<Highway> outgoing;
+    private List<Truck> in_city;
 }
--- src/Restocking/Highway.java
+++ src/Restocking/Highway.java
@@ -1,11 +1,28 @@
 package Restocking;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Highway {
     private City origin;
     private City target;
-    private int capacity;
     private int length;
+    private int capacity;
     private List<Truck> fill;
+
+    public Highway(City org, City tgt, int len, int cap) {
+        origin = org;
+        target = tgt;
+        length = len;
+        capacity = cap;
+        fill = new LinkedList<>();
+    }
+
+    public void addTruck(Truck t) {
+        fill.add(t);
+    }
+
+    public Truck removeTruck() {
+        return fill.remove(0);
+    }
 }
