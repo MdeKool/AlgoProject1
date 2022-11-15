a4eb36464a715ca4faec235dc1afd28ca8101861
MdeKool <matthijsdekool@gmail.com>
2022-11-15 10:48:32 +0100

    Finished std input

--- src/Restocking/Main.java
+++ src/Restocking/Main.java
@@ -10,9 +10,19 @@ public class Main {
             File infile = new File(args[0]);
             Scanner reader = new Scanner(infile);
 
+            // Read first input line for #Locations, #Highways and Time.
+            String[] l1 = reader.nextLine().split(" ");
+            Graph g = new Graph(Integer.parseInt(l1[0]), Integer.parseInt(l1[2]));
+
+            // Create highways
             while (reader.hasNextLine()) {
-                System.out.println(reader.nextLine());
+                String[] input = reader.nextLine().split(" ");
+                g.makeHighway(Integer.parseInt(input[0]),
+                            Integer.parseInt(input[1]),
+                            Integer.parseInt(input[2]),
+                            Integer.parseInt(input[3]));
             }
+
         } catch(IOException e) {
             System.out.println("An IOException occurred");
             e.printStackTrace();
