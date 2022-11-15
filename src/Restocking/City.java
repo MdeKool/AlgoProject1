b97bead4b3c84bbcb7c0b3437330ccdd398b8f61
MdeKool <matthijsdekool@gmail.com>
2022-11-15 10:35:08 +0100

    Added constructor and methods to City.java

--- src/Restocking/City.java
+++ src/Restocking/City.java
@@ -1,9 +1,31 @@
 package Restocking;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class City {
     private List<Highway> incoming;
     private List<Highway> outgoing;
     private List<Truck> in_city;
+
+    public City() {
+        incoming = new LinkedList<>();
+        outgoing = new LinkedList<>();
+        in_city = new LinkedList<>();
+    }
+    public void addIncoming(Highway hwy) {
+        incoming.add(hwy);
+    }
+
+    public void addOutgoing(Highway hwy) {
+        outgoing.add(hwy);
+    }
+
+    public void enter(Truck t) {
+        in_city.add(t);
+    }
+
+    public Truck leave(Truck t) {
+        return in_city.remove(0);
+    }
 }
