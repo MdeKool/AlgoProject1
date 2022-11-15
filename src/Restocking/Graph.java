e394a7c03647dd273c16cceb44db26e393c3eeb9
MdeKool <matthijsdekool@gmail.com>
2022-11-15 10:34:19 +0100

    Added Class to represent graph

---
+++ src/Restocking/Graph.java
@@ -0,0 +1,26 @@
+package Restocking;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Graph {
+
+    private List<City> cities;
+    private List<Highway> highways;
+    private int time;
+
+    public Graph(int cit, int tim) {
+        cities = new LinkedList<>();
+        for (int i = 0; i < cit; i++) {
+            cities.add(new City());
+        }
+        highways = new LinkedList<>();
+        time = tim;
+    }
+
+    public void makeHighway(int cit1, int cit2, int len, int cap) {
+        Highway hwy = new Highway(cities.get(cit1), cities.get(cit2), len, cap);
+        cities.get(cit1).addOutgoing(hwy);
+        cities.get(cit2).addIncoming(hwy);
+    }
+}
