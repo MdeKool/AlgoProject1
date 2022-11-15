3e6df3a4976b4323e88e9f236ed0ee13fdf8eb95
MdeKool <matthijsdekool@gmail.com>
2022-11-15 10:38:30 +0100

    Added std input to Main.java

--- src/Restocking/Main.java
+++ src/Restocking/Main.java
@@ -1,7 +1,21 @@
 package Restocking;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            File infile = new File(args[0]);
+            Scanner reader = new Scanner(infile);
+
+            while (reader.hasNextLine()) {
+                System.out.println(reader.nextLine());
+            }
+        } catch(IOException e) {
+            System.out.println("An IOException occurred");
+            e.printStackTrace();
+        }
     }
 }
