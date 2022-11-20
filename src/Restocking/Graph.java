3c7660ba327d6937257f8ec8c2bd2f3f03c575d6
Mark Boute <bmark0702@gmail.com>
2022-11-20 14:42:14 +0100

    add comments

--- src/Restocking/Graph.java
+++ src/Restocking/Graph.java
@@ -1,8 +1,6 @@
 package Restocking;
 
 import java.util.*;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Graph {
     private final int cities;
@@ -11,6 +9,7 @@ public class Graph {
     private final Map<String, Integer> highwaysMap;
     private final List<List<Highway>> highways;
     private final List<List<Highway>> flow;
+    private int shortest_path = MAX_VALUE;
 
     public Graph(int cities, int time) {
         this.cities = cities;
@@ -48,37 +47,87 @@ public class Graph {
         }
     }
 
-    public void preprocess() {
+    public int run() {
+        this.preprocess();
+        return this.edmondsKarp();
+    }
+
+    private void preprocess() {
         this.find_paths(0, 0, new Highway(0, 0, 0, 0, MAX_VALUE, MAX_VALUE));
-        for (List<Highway> city : this.highways) {
-            city.removeIf(h -> h.min_dist_to_dest() < 0 || h.fastest_path() > this.time);
+        for (List<Highway> city : this.highways) { // removed legacy check:  h.min_dist_to_dest() < 0 ||
+            city.removeIf(h -> h.min_dist_to_dest() > this.time || h.fastest_path() > this.time);
         }
         System.out.println("Prune result:");
         System.out.println(this);
+        create_time_expanded_graph();
     }
 
-    public void process() {
+    private int find_paths(int source, int length, Highway pred) {
+        if (length > this.time) // no path with length < time
+            return -1;
+
+        if (source == cities - 1) { // destination reached, update accordingly
+            pred.set_min_dist_to_dest(pred.length());
+            pred.set_fastest_path(Math.min(pred.fastest_path(), length));
+            this.shortest_path = Math.min(this.shortest_path, pred.fastest_path());
+            return pred.length();
+        }
+
+        int shortest_child_path = MAX_VALUE;
+        int fastest_path = MAX_VALUE;
+        for (Highway highway : this.highways.get(source))
+        { // for each highway leaving city[source]
+            int result;
+            // if a search that is minimally as deep as we can search has already been done, re-use that result
+            if ( highway.min_dist_to_dest() >= MAX_VALUE
+                    || highway.fastest_path() - highway.min_dist_to_dest() > length) {
+                result = find_paths(highway.to(), length + highway.length(), highway);
+            } else { // otherwise: search
+                result = highway.min_dist_to_dest();
+            }
+            if (result < 0) continue; // ignore path with no solution
+
+            // minimize on result
+            shortest_child_path = Math.min(shortest_child_path, result);
+            fastest_path = (Math.min(fastest_path, highway.fastest_path()));
+        }
+
+        if (shortest_child_path > 0 ) {
+            pred.set_min_dist_to_dest(shortest_child_path + pred.length());
+        } else { // negative weight cycle detected
+            System.out.println(source + " has a negative shortest path, negative weight cycle?");
+        }
+        pred.set_fastest_path(fastest_path);
+        return shortest_child_path + pred.length();
+    }
+
+    private void create_time_expanded_graph() {
         BitSet visited = new BitSet(this.cities*this.time);
         for (int t = 0; t < this.time; t++) {
             visited.set(t*this.cities);
         }
 
         int added = 0;
-        for (int c = 0; c < this.cities; c++) {
-            for (int t = 0; t < this.time; t++) {
-                if (visited.get(c+t*this.cities)) {
-                    for (Highway h : this.highways.get(c)) {
-                        if (this.time >= t + h.min_dist_to_dest()) {
-                            List<Highway> lh = this.flow.get(c+t*this.cities);
-                            Highway nh = new Highway(c+t*this.cities,
-                                    h.to() + (t + h.length()) * this.time,
-                                    h.capacity(),
-                                    h.length(),
-                                    h.min_dist_to_dest(),
-                                    h.fastest_path());
-                            lh.add(nh);
+        for (int c = 0; c < this.cities; c++)
+        { // for each city
+            for (int t = 0; t < this.time - this.shortest_path; t++)
+            { // for each possible time-frame (do not run time for which no paths exist.
+                if (visited.get(c+t*this.cities))
+                { // if city_time 'c_t' has been visited
+                    for (Highway h : this.highways.get(c))
+                    { // for each outgoing highway
+                        if (this.time >= t + h.min_dist_to_dest())
+                        { // if destination can still be reached using this highway
+                            // add highway from city_time to dest_time+length
+                            this.flow.get(c+t*this.cities).add(
+                                    new Highway(c+t*this.cities,
+                                        h.to() + (t+h.length())*this.time,
+                                        h.capacity(), h.length(),
+                                        h.min_dist_to_dest(), h.fastest_path()
+                                    )
+                            ); added++;
+                            // mark dest_time+length as visited
                             visited.set(h.to()+(t+h.length())*this.cities);
-                            added++;
                         }
                     }
                 }
@@ -95,44 +144,7 @@ public class Graph {
         }
     }
 
-    private int find_paths(int source, int length, Highway pred) {
-        if (length > this.time) { // no path
-            return -1;
-        }
-
-        if (source == cities - 1) {
-            pred.set_min_dist_to_dest(pred.length());
-            pred.set_fastest_path(Math.min(pred.fastest_path() ,length));
-            return pred.length();
-        }
-
-        int shortest_child_path = MAX_VALUE;
-        int fastest_path = MAX_VALUE;
-        for (Highway highway : this.highways.get(source)) {
-
-            // discover
-            int result;
-            if ( highway.min_dist_to_dest() >= MAX_VALUE
-                    || highway.fastest_path() - highway.min_dist_to_dest() > length) {
-                result = find_paths(highway.to(), length + highway.length(), highway);
-            } else {
-                result = highway.min_dist_to_dest();
-            }
-
-            if (result < 0) continue; // ignore path with no solution
-
-            shortest_child_path = Math.min(shortest_child_path, result);
-            fastest_path = (Math.min(fastest_path, highway.fastest_path()));
-        }
-
-        if (shortest_child_path > 0 ) {
-            pred.set_min_dist_to_dest(shortest_child_path + pred.length());
-        }
-        pred.set_fastest_path(fastest_path);
-        return shortest_child_path + pred.length();
-    }
-
-    public int edmondsKarp() {
+    private int edmondsKarp() {
         int max_flow = 0;
 
         int[] sources = new int[this.time];
